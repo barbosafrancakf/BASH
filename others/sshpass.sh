402b68f0db3e6b7bef1b80510b7dce004ec97f1a
Barbosa <barbosafrancakf@hotmail.com>
2015-07-25 03:09:27 +0100

    sshpass bash scripting
    This script aims to copy files between two Linux machines.
    
    **Procedures:**
    
    *list linux updates
    *install linux updates
    *install zip library
    *install sshpass library
    *ask local directory to compress
    *ask remote server details to connect
    *compress local files
    *copy local files to remote server
    *write readme.txt file

--- others/sshpass.sh
+++ others/sshpass.sh
@@ -9,16 +9,16 @@ echo -e "${Green}[UPDATES]${STD} Installing LINUX ${Green}Z${RED}I${STD}P librar
 apt-get -qy install zip &> /dev/null
 echo -e "${Green}[UPDATES]${STD} Installing LINUX ${Green}SS${RED}H${STD}PASS libraries..."
 apt-get -qy install sshpass &> /dev/null
-echo -e "${Green}[SETUP]${STD} Enter local directory do you want to upload: (${RED}/var/www/${STD})"   
+echo -e "${Green}[SETUP]${STD} Enter local directory do you want to upload: (${RED}/var/www/${STD})"
 read ldirectory 
-echo -e "${Green}[SETUP]${STD} Enter server address to upload your backup files: (${RED}127.0.0.1${STD})"   
+echo -e "${Green}[SETUP]${STD} Enter server address to upload your backup files: (${RED}127.0.0.1${STD})"
 read eaddress
-echo -e "${Green}[SETUP]${STD} Enter server user to login: (${RED}root${STD})"   
+echo -e "${Green}[SETUP]${STD} Enter server user to login: (${RED}root${STD})"
 read euser
-echo -e "${Green}[SETUP]${STD} Enter server $euser password to complete logging into your server: "   
+echo -e "${Green}[SETUP]${STD} Enter server $euser password to complete logging into your server: "
 read epassword
 zip -r $date.zip $ldirectory &> /dev/null
-echo -e "${Green}[SETUP]${STD} Extracting local files..."   
-echo -e "${Green}[SETUP]${STD} Copying files to remote server directory..."   
+echo -e "${Green}[SETUP]${STD} Extracting local files..."
+echo -e "${Green}[SETUP]${STD} Copying files to remote server directory..."
 sshpass -p $epassword ssh -o StrictHostKeyChecking=no $euser@$eaddress echo $'Your server was remotely visited during a file extraction process. > readme.txt'
 sshpass -p $epassword scp $date.zip $euser@$eaddress:/root/ 
